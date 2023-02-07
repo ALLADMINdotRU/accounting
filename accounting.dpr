e8c85116ac2b45a4a2508616b42b3e74d329e20e
ALLADMINdotRU <fil-23@ya.ru>
2023-02-08 01:16:47 +0700

    test

---
+++ accounting.dpr
@@ -0,0 +1,14 @@
+program accounting;
+
+uses
+  Vcl.Forms,
+  fmStart in 'fmStart.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
